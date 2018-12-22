cf3f6f707940cff9eac778a012c8c8e00396c5ce
venkat <venkat@venkat-PC>
2018-12-22 12:19:06 +0530

    updated batch file

--- Myproj/schedule.bat
+++ Myproj/schedule.bat
@@ -1,5 +1,5 @@
-set classpath=d:\sel_dec18\Myproj\bin;d:\sel_dec18\Library\*
+set classpath=d:\sel_dec18\mylocalrep\Myproj\bin;d:\sel_dec18\Library\*
 
-java org.testng.TestNG d:\sel_dec18\Myproj\src\yahoo_testing.xml
+java org.testng.TestNG d:\sel_dec18\mylocalrep\Myproj\src\yahoo_testing.xml
 
 pause
